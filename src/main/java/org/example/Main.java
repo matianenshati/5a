e244737c6a14650f8489d95f839ada47b6f0fbe8
matianenshati <mnenshati@gmail.com>
2026-09-14 10:37:30 +0200

    hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
